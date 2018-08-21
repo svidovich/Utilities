2b408dc537c6c0cbfbaf6b19af272fcba2b71e6f
vivec <samuel.vidovich@gmail.com>
2018-08-20 22:48:14 -0400

    All the organizational things I want to do

--- desktopcleanup.py
+++ desktopcleanup.py
@@ -5,3 +5,9 @@ print(ls)
 
 if 'images' not in ls:
 	os.mkdir("images")
+if 'archives' not in ls:
+	os.mkdir("archives")
+if 'books' not in ls:
+	os.mkdir("books")
+if 'scripts' not in ls:
+	os.mkdir("scripts")
