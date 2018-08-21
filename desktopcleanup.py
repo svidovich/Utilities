971056f14136c69844d354b0eef8f65d2c31e1b2
vivec <samuel.vidovich@gmail.com>
2018-08-20 22:52:18 -0400

    I can pwd!

--- desktopcleanup.py
+++ desktopcleanup.py
@@ -1,8 +1,9 @@
 # /usr/bin/env python
 import os
 ls = os.listdir(".")
+pwd = os.getcwd()
 print(ls)
-
+print(pwd)
 if 'images' not in ls:
 	os.mkdir("images")
 if 'archives' not in ls:
@@ -11,3 +12,4 @@ if 'books' not in ls:
 	os.mkdir("books")
 if 'scripts' not in ls:
 	os.mkdir("scripts")
+
