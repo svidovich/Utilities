db5196649b66ddd1e769415d864c24449341b157
vivec <samuel.vidovich@gmail.com>
2018-08-21 23:18:50 -0400

    Better extension matching

--- desktopcleanup.py
+++ desktopcleanup.py
@@ -22,25 +22,25 @@ for result in ls:
 		os.rename(current, moved)
 
 for result in ls:
-	if any(x in result for x in['.zip', '.gz', '.tar', '.bz2', '.lz', '.7z', '.cab', '.jar', '.rar', '.tgz', '.zipx']):
+	if any(x in result.lower() for x in['.zip', '.gz', '.tar', '.bz2', '.lz', '.7z', '.cab', '.jar', '.rar', '.tgz', '.zipx']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'archives', result)
 		os.rename(current, moved)
 
 for result in ls:
-	if any(x in result for x in['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt']):
+	if any(x in result.lower() for x in['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'documents', result)
 		os.rename(current, moved)
 
 for result in ls:
-	if any(x in result for x in['.sh', '.ps1', '.x']):
+	if any(x in result.lower() for x in['.sh', '.ps1', '.x']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'scripts', result)
 		os.rename(current, moved)
 
 for result in ls:
-	if any(x in result for x in['.wav', '.ogg', '.mp3', '.flac', '.m4a','.wma','.vox']):
+	if any(x in result.lower() for x in['.wav', '.ogg', '.mp3', '.flac', '.m4a','.wma','.vox']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'music', result)
 		os.rename(current, moved)
