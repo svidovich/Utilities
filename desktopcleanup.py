877efc7c3412755ed5a0c7e0ccdcb633693587dd
vivec <samuel.vidovich@gmail.com>
2018-08-21 23:27:59 -0400

    Added applications support

--- desktopcleanup.py
+++ desktopcleanup.py
@@ -13,6 +13,8 @@ if 'scripts' not in ls:
 	os.mkdir("scripts")
 if 'music' not in ls:
 	os.mkdir("music")
+if 'applications' not in ls:
+	os.mkdir("applications")
 
 
 for result in ls:
@@ -22,7 +24,7 @@ for result in ls:
 		os.rename(current, moved)
 
 for result in ls:
-	if any(x in result.lower() for x in['.zip', '.gz', '.tar', '.bz2', '.lz', '.7z', '.cab', '.jar', '.rar', '.tgz', '.zipx']):
+	if any(x in result.lower() for x in['.zip', '.gz', '.tar', '.bz2', '.lz', '.7z', '.cab', '.rar', '.tgz', '.zipx']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'archives', result)
 		os.rename(current, moved)
@@ -43,4 +45,11 @@ for result in ls:
 	if any(x in result.lower() for x in['.wav', '.ogg', '.mp3', '.flac', '.m4a','.wma','.vox']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'music', result)
+		os.rename(current, moved)
+
+# I put jar here because they're typically executable java applications.
+for result in ls:
+	if any(x in result.lower() for x in['.appimage', '.exe', '.dmg', '.jar']):
+		current = os.path.join(pwd, result)
+		moved = os.path.join(pwd, 'applications', result)
 		os.rename(current, moved)
