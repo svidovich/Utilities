ec44e154d07ebcd8e8b9ccb5d1cd1a53239ec940
vivec <samuel.vidovich@gmail.com>
2018-08-21 23:17:39 -0400

    added music support

--- desktopcleanup.py
+++ desktopcleanup.py
@@ -11,11 +11,12 @@ if 'documents' not in ls:
 	os.mkdir("documents")
 if 'scripts' not in ls:
 	os.mkdir("scripts")
-
+if 'music' not in ls:
+	os.mkdir("music")
 
 
 for result in ls:
-	if any(x in result for x in['jpg', 'png', 'gif', 'tif']):
+	if any(x in result.lower() for x in['jpg', 'png', 'gif', 'tif']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'images', result)
 		os.rename(current, moved)
@@ -30,4 +31,16 @@ for result in ls:
 	if any(x in result for x in['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt']):
 		current = os.path.join(pwd, result)
 		moved = os.path.join(pwd, 'documents', result)
+		os.rename(current, moved)
+
+for result in ls:
+	if any(x in result for x in['.sh', '.ps1', '.x']):
+		current = os.path.join(pwd, result)
+		moved = os.path.join(pwd, 'scripts', result)
+		os.rename(current, moved)
+
+for result in ls:
+	if any(x in result for x in['.wav', '.ogg', '.mp3', '.flac', '.m4a','.wma','.vox']):
+		current = os.path.join(pwd, result)
+		moved = os.path.join(pwd, 'music', result)
 		os.rename(current, moved)
