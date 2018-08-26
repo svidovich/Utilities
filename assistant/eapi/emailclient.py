bd40be88b96ec932bb7931085143bee9bcb05228
vivec <samuel.vidovich@gmail.com>
2018-08-25 21:17:55 -0400

    Added message constructor, moved parts of initializer out

--- assistant/eapi/emailclient.py
+++ assistant/eapi/emailclient.py
@@ -16,19 +16,13 @@ emailmessage = "message.email"
 # Takes
 # > account: json file with schema
 # >> { login:email, password:emailpassword }	
-# > recipient: json file with schema
-# >> { destination:recipientemailaddress )
-# > emailmessage: json file with schema
-# >> { subject:emailsubjectline, body:emailbodytosend }
+# Returns
+# N/A
 def initialize_client(account, recipient, emailmessage):
 	# Set up a server that we can use
 	server = smtplib.SMTP('smtp.gmail.com',587)
 	with open(account, 'r') as file:
 		details = json.load(file)
-	with open(recipient, "r") as file:
-		destination = json.load(file)
-	with open(emailmessage, "r") as file:
-		message = json.load(file)
 	# Let's begin talking with the server
 	try:
 		server.starttls()
@@ -37,13 +31,29 @@ def initialize_client(account, recipient, emailmessage):
 		print("Exception occured: {}".format(e))
 		server.quit()
 
-
-msg = MIMEMultipart()
-msg['From'] = details["login"]
-msg['To'] = destination["destination"]
-msg['Subject'] = message["subject"]
-msg.attach(MIMEText(message["body"], 'plain'))
-msg = msg.as_string()
+# This ( message builder )
+# Takes
+# > recipient: json file with schema
+# >> { destination:recipientemailaddress )
+# > emailmessage: json file with schema
+# >> { subject:emailsubjectline, body:emailbodytosend }
+# Returns
+# > email message according to python MIME lib as string
+def construct_message(recipient, emailmessage):
+	try:
+		with open(recipient, "r") as file:
+			destination = json.load(file)
+		with open(emailmessage, "r") as file:
+			message = json.load(file)
+	except Exception as e:
+		print("An Exception occured when constructing an email message: {}".format(e))
+	msg = MIMEMultipart()
+	msg['From'] = details["login"]
+	msg['To'] = destination["destination"]
+	msg['Subject'] = message["subject"]
+	msg.attach(MIMEText(message["body"], 'plain'))
+	msg = msg.as_string()
+	return msg
 
 try:
 	print("Sending email message.")
