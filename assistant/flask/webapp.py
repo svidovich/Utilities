9074a8d994399c282e60cda38012f2bb9cb553e7
vivec <samuel.vidovich@gmail.com>
2018-08-26 21:27:12 -0400

    use json methods instead of writing strings like a tool

--- assistant/flask/webapp.py
+++ assistant/flask/webapp.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 import sys
 import os
 import shutil
+import json
 from eapi.emailclient import *
 
 app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
@@ -18,10 +19,11 @@ def assistant_application():
 	recipient = "./eapi/destination.cfg"
 	emailmessage = "message.email"
 	server = initialize_client(account)
-	subject = request.form.get('subject')
-	body = request.form.get('body')
+	emailsubject = request.form.get('subject')
+	emailbody = request.form.get('body')
+	emaildata = { "subject":emailsubject, "body":emailbody }
 	with open(emailmessage, 'w') as file:
-		file.write('{"subject":"{}", "body":"{}"'.format(subject, body))
+		file.write(json.dumps(emaildata))
 	message = construct_message(account, recipient, emailmessage)
 	send_message(account, recipient, message, server)
 	close_connection(server)
