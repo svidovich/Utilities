aa48a2eda5942b676ad4807580afaf445d4c1354
vivec <samuel.vidovich@gmail.com>
2018-08-27 22:29:48 -0400

    Pistis Sophia Spider skeleton

---
+++ assistant/scrapyfiles/projectfiles/spiders/pistis-sophia.py
@@ -0,0 +1,28 @@
+from scrapy import Spider
+from scrapy.http import Request, FormRequest, HtmlResponse
+import urlparse
+import re
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class pistisSophiaSpider(Spider):
+    name = "pistissophia"
+    ls = os.listdir(".")
+    pwd = os.getcwd()
+    if 'archives' not in ls:
+	os.mkdir("archives")
+
+    # yapf: disable
+    custom_settings = {
+        # This is important! It allows us to use the download links from the pages to get files!
+        'MEDIA_ALLOW_REDIRECTS': 'True',
+        }
+    # yapf: enable
+    allowed_domains = ["gnosis.org"]
+    def start_requests(self):
+        url = "http://www.gnosis.org/library/pistis-sophia/index.htm"
+        yield Request(url=url, callback=self.search_library)
+
