d9a3d30a51179eaeefc5f479b3b803a7bce46edf
vivec <samuel.vidovich@gmail.com>
2018-09-06 22:42:01 -0400

    finished lister tool c:

--- ytlister/scrapyfiles/projectfiles/spiders/lister.py
+++ ytlister/scrapyfiles/projectfiles/spiders/lister.py
@@ -4,6 +4,7 @@ from scrapy.http import Request, FormRequest, HtmlResponse
 import urlparse
 import re
 import os
+import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -49,15 +50,15 @@ class listerSpider(Spider):
 					})
 	def parse_playlist(self, response):
 		vidlist = response.xpath('//td[@class="pl-video-title"]').extract()
+		playlistdictionaries = []
 		for html in vidlist:
 			entry = {}
 			entry['title'] = html.split('</a>')[0].split('>')[2].strip()
 			for href in re.findall('href="(.*)"', html):
 				if not any( z in href for z in ['user', 'channel']):
 					entry['link'] = "https://www.youtube.com{}".format(href.split('&amp')[0])
-			print(entry)
-
-
-#current = os.path.join(self.pwd, title)
-#moved = os.path.join(self.pwd, 'archives', title)
-#os.rename(current, moved)
+			playlistdictionaries.append(entry)
+		print(playlistdictionaries)
+		filename = "myplaylist.json"
+		with open(filename, "w+") as file:
+			json.dump(playlistdictionaries, file)
