93eee4442402c598169cbeb9bfcd6fd15958d264
vivec <samuel.vidovich@gmail.com>
2018-09-12 22:52:33 -0400

    expand requirements

--- assistant/install.sh
+++ assistant/install.sh
@@ -1,6 +1,14 @@
 dir=$(pwd)
 cd $dir
+# Make executables
+chmod +x run.sh
 cd flask/
 chmod +x run_dev.sh
 cd eapi/
 chmod +x runtest.py
+
+# Get dependencies
+sudo apt install -y python
+sudo apt install -y python-pip python-dev build-essential
+sudo pip install --upgrade pip
+pip install flask
---
+++ assistant/run.sh
@@ -0,0 +1,4 @@
+dir=$(pwd)
+cd $dir
+cd flask/eapi/
+python runtest.py
